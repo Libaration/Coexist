54c6dcf52bd383f2dcbe73e0db3fc7a06e923b14
Cristian Deleon <libaration@gmail.com>
2021-10-15 02:01:38 -0400

    enable hot reloading

---
+++ app/helpers/api/v1/coexist/users_helper.rb
@@ -0,0 +1,2 @@
+module Api::V1::Coexist::UsersHelper
+end
--- app/javascript/packs/index.ts
+++ app/javascript/packs/index.ts
@@ -2,25 +2,15 @@
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
-
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.body.appendChild(document.createElement('div'))
+  );
+});
